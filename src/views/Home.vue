7e78323a0e15390bc9b5ace6cb68f0b8d9765edd
igor-de-andrade <cadastros.igordeandrade@gmail.com>
2025-12-05 21:33:46 -0300

    Adiciona a estrutura base do projeto

---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Seja bem-vindo à Petly! 🐶💚😺</h1>
+  <p>Nossa página está em construção. Em breve nossos serviços estarão disponíveis!</p>
+</template>
+
+<style scoped></style>
